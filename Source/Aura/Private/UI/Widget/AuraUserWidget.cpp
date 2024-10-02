d96805670621350908da2e6ead44f9ffd7158da8
human004 <timothybenton@gmail.com>
2024-10-02 12:31:09 -0700

    add ui widget and controller to cpp

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Timothy Benton 2024
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
